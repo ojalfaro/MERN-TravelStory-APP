d00b8d912500b094e19cc718a0d35f515fd65654
Javier Alfaro <ojalfarob@gmail.com>
2024-09-19 12:22:49 -0600

    add project

---
+++ frontend/travel-story-app/src/pages/Auth/Login.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import PasswordInput from '../../components/Input/PasswordInput.jsx'
+import { useNavigate } from 'react-router-dom';
+
+ const Login = ()  => {
+
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [error,setError] = useState(null);
+
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {}
+
+  return (
+    <div className='h-screen bg-cyan-50 overflow-hidden relative'>
+
+      <div className='login-ui-box right-10 -top-40' />
+      <div className='login-ui-box bg-cyan-200 -bottom-40 right-1/2' />
+
+      <div className='container h-screen flex items-center justify-center px-20 mx-auto'>
+        <div className='w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50'>
+          <div>
+            <h4 className='text-5xl text-white font-semibold leading-[58px]'>
+              capture your <br/>Journeys
+            </h4>
+            <p className='text-[15px] text-white leading-6 pr-7 mt-4'> 
+              Record your travel experiences and memories in your personal
+              traverl journal.
+            </p>
+          </div> 
+        </div>
+
+        <div className='w-2/4 h[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20'>
+          <form onSubmit={handleLogin}>
+            <h4 className='text-2xl font-semibold mb-7'>login</h4>
+            <input type='text' placeholder='Email' className='input-box'
+            value={email}
+            onChange={({target}) => {
+              setEmail(target.value);
+            }}
+            />
+
+            <PasswordInput 
+            value={password}
+            onChange={({target}) => {
+              setPassword(target.value);
+            }}/>
+
+            <button type='submit' className='btn-primary'>
+              Login
+            </button>
+            <p className='text-xs text-slate-500 text-center my-4'>Or</p>
+            <button 
+            type='submit'
+            className='btn-primary btn-light'
+            onClick={() => {
+              navigate("/signUp")
+            }}
+            >
+              CREATE ACOUNT
+            </button>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
