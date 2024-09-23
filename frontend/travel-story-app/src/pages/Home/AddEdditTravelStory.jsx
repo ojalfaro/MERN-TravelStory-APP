a5a0833d02c5cba52255dceef31026c6f6cc277c
Javier Alfaro <ojalfarob@gmail.com>
2024-09-23 11:43:18 -0600

    add calendar - modal stories card

--- frontend/travel-story-app/src/pages/Home/AddEdditTravelStory.jsx
+++ frontend/travel-story-app/src/pages/Home/AddEdditTravelStory.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdAdd,MdDeleteOutline,MdUpdate,MdClose } from 'react-icons/md'
+import DateSelector from '../../components/Input/DateSelector'
 
 const AddEdditTravelStory = ({
     storyInfo,
@@ -8,6 +9,11 @@ const AddEdditTravelStory = ({
     getAllTravelsStories
 }) => {
 
+    const [title,setTitle] = useState("")
+    const [storyimg,setStoryimg] = useState(null)
+    const [story,setStory] = useState("")
+    const [visitedLocation,setVisitedLocation] = useState([])
+    const [visitedDate,setVisitedDate] = useState(null)
     const handleAddOrUpdateClick = () => {}
   return (
     <div>
@@ -39,6 +45,25 @@ const AddEdditTravelStory = ({
                     </button>
 
                 </div>
+            </div>
+        </div>
+
+        <div>
+            <div className='flex-1 flex flex-col gap-2 pt-4'>
+                <label className='input-label'>Title</label>
+                <input 
+                type='text'
+                className='text-2xl text-slate-950 outline-none'
+                placeholder='A day at the great wall'
+                value={title}
+                onChange={({target }) => setTitle(target.value) }
+                />
+
+                <div className='my-3'>
+                    <DateSelector date={visitedDate} setDate={setVisitedDate} />
+
+                </div>
+
             </div>
         </div>
     </div>
